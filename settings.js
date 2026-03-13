67ba7a05c5be5e1e9896bce058fd47acb97a2af7
VIKRAM KUMAR <vikramkumarart@gmail.com>
2026-03-13 13:00:18 +0530

    Update owner number and version in settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const settings = {
   author: '‎',
   botName: "CODE_BREAKER",
   botOwner: 'Vikram_Laheri', // Your name
-  ownerNumber: '8825297632', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '918825297632', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.8",
-  updateZipUrl: "https://github.com/Biharkebahubali/CODE-BREAKER-BOT/archive/refs/heads/main.zip",
+  version: "5.0.9",
+  updateZipUrl: "https://github.com/Biharkebahubali/CODE-BREAKER-BOT.git",
 };
 
 module.exports = settings;
